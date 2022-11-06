f75679ec8474c21b194d7d3add06e1001e82c429
User <alexust11@yandex.ru>
2022-11-06 20:51:22 +0700

    Задача 1.Билет 17 готова

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String word = "Комокс";
-        if (word.equalsIgnoreCase(new StringBuffer(word).reverse().toString())) {
-            System.out.println("Полиндром");
-        } else { System.out.println("Не полиндром"); }
+        if (word.equalsIgnoreCase(new StringBuffer(word).reverse().toString())) { System.out.println("Полиндром");}
+        else { System.out.println("Не полиндром"); }
     }
 }
